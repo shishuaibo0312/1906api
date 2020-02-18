8007579abbdb4ad56f513f57f78bfe45534d0e5d
shishuaibo0312 <2565918843@qq.com>
2020-02-18 00:48:56 +0000

    debug

--- app/Http/Controllers/Test/GoodsController.php
+++ app/Http/Controllers/Test/GoodsController.php
@@ -5,38 +5,61 @@ namespace App\Http\Controllers\Test;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Model\GoodsModel;
+use Illuminate\Support\Facades\Redis;
 class GoodsController extends Controller
 {
     //商品页面的访问量
      	function goods(){
+
+
+     		$goodsinfo=GoodsModel::orderBy('id', 'desc')->limit(10)->get()->toArray();
+     		//dd($datainfo);
+     		$datanumber=count($goodsinfo);
+     		//dd($datanumber);
+     		if($datanumber>=10){
+     		//dd($datainfo);
+     		$datafirst=$goodsinfo[count($goodsinfo)-1];
+     		$time=time()-strtotime($datafirst['created_at']);
+     		//dump($time);
+     		if($time<28860){
+     		 	//dd($time);
+     		 	echo "1分钟内访问不能大于10次";die;
+     		}
+
+     		}
+     		
      		$goods_name='联想电脑';
      		echo "商品名称:".$goods_name;
      		echo "<br>";
 
      		$uv=$_SERVER['HTTP_USER_AGENT'];
-     		//echo "uv:".$uv;
-     		echo "<br>";
+     		
 
      		$ip=$_SERVER['REMOTE_ADDR'];
      		//echo "ip:".$ip;
      		
-     		
+     		   		
      		//存入数据库
-     		$info=GoodsModel::where('uv','=',$uv)->first();
-     		//dd($info);
-     		if($info){
-     			GoodsModel::where('uv','=',$uv)->update(['pv'=>$info['pv']+1]);
-     			$pv=$info['pv']+1;
-     			echo "访问量：".$pv;
-     		}else{
-     			$data['ip']=$ip;
-	     		$data['goods_name']=$goods_name;
-	     		$data['pv']=1;
-	     		$data['uv']=$uv;
-	     		$data['created_at']=time();
-	     		$id=GoodsModel::insertGetId($data);
-	     		echo "访问量：".$data['pv'];
-     		}
+ 			$data['ip']=$ip;
+     		$data['goods_name']=$goods_name;
+     		$data['uv']=$uv;
+     		GoodsModel::insert($data);
+     		
+     		$count=GoodsModel::count();
+     		echo "访问量pv：".$count;
+     		
+     		
+     		
+ 			echo "<br>";
+
+ 			$info=GoodsModel::get()->toArray();
+     		$uvinfo=array_column($info, 'uv');
+     		$uvinfo=array_unique($uvinfo);
+     		//dd($uvinfo);
+     		$uvcount=count($uvinfo);
+     		echo "uv:".$uvcount;
+     		
+ 		   
      		
      	}
 }
